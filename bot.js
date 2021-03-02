ed0b21598574400faa5da834cc62d7df89d13f5e
AmishAlvi <amish.alvi@gmail.com>
2021-03-02 14:45:45 +0300

    bot guild role

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ bot.on('message', msg => {
       });
 
     var role = msg.guild.roles.cache.get("🔴 offbeats live now");;
-    bot.user.member.roles.add(role).catch(console.error);
+    bot.guild.member.roles.add(role).catch(console.error);
   } else if (msg.content.startsWith('!notlive')) {
     bot.user.setActivity("offbeats", {
         type: "GAME",
